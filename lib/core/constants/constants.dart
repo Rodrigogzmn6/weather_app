7e5dd32782ff11ec5346a528627a2db02a7f50bb
Rodrigo Guzman <rodrigo.guzman@sovos.com>
2023-09-08 19:10:48 -0300

    Remote data source implemented

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,11 +1,11 @@
+import 'package:weather_app/core/auth/keys.dart';
+
 class Constants {
   // * API Calls
-  static const String localWeatherApi =
-      'https://api.openweathermap.org/data/2.5/weather?units=metric';
-  // &lat=57&lon=-2.15&appid={API key}';
-  static const String cityWeatherApi =
-      'https://api.openweathermap.org/data/2.5/weather?units=metric';
-  // &q={city name}&appid={API Key}';
+  static const String weatherApi =
+      'https://api.openweathermap.org/data/2.5/weather?appid=$openWeatherApi&units=metric';
+  // &lat=57&lon=-2.15';
+
   // * Shared Preferences
   static const String cachedWeather = 'CACHED_WEATHER';
 }
--- lib/features/weather/data/data_sources/weather_remote_data_source.dart
+++ lib/features/weather/data/data_sources/weather_remote_data_source.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:geolocator/geolocator.dart';
 import 'package:weather_app/core/constants/constants.dart';
+import 'package:weather_app/core/error/exception.dart';
 import 'package:weather_app/features/weather/data/models/weather_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -20,14 +24,51 @@ class WeatherRemoteDataSourceImpl implements WeatherRemoteDataSource {
   WeatherRemoteDataSourceImpl({required this.client});
 
   @override
-  Future<WeatherModel> getCityWeather(String city) {
-    client.get(Uri.parse(Constants.cityWeatherApi));
-    throw UnimplementedError();
+  Future<WeatherModel> getCityWeather(String city) async {
+    final url = '${Constants.weatherApi}&q=$city';
+    return _getWeatherFromUrl(url);
   }
 
   @override
-  Future<WeatherModel> getLocalWeather() {
-    // TODO: implement getLocalWeather
-    throw UnimplementedError();
+  Future<WeatherModel> getLocalWeather() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    final url =
+        '${Constants.weatherApi}&lat=${position.latitude}&lon=${position.longitude}';
+    return await _getWeatherFromUrl(url);
   }
+
+  Future<WeatherModel> _getWeatherFromUrl(String url) async {
+    final response = await client.get(
+      Uri.parse(url),
+    );
+    return response.statusCode == 200
+        ? WeatherModel.fromJson(jsonDecode(response.body))
+        : throw ServerException();
+  }
+  //! Método para verificar que se otorga permisos, usar en UI
+  // Future<bool> _handleLocationPermission() async {
+  // bool serviceEnabled;
+  // LocationPermission permission;
+
+  // serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  // if (!serviceEnabled) {
+  //   ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+  //       content: Text('Location services are disabled. Please enable the services')));
+  //   return false;
+  // }
+  // permission = await Geolocator.checkPermission();
+  // if (permission == LocationPermission.denied) {
+  //   permission = await Geolocator.requestPermission();
+  //   if (permission == LocationPermission.denied) {
+  //     ScaffoldMessenger.of(context).showSnackBar(
+  //         const SnackBar(content: Text('Location permissions are denied')));
+  //     return false;
+  //   }
+  // }
+  // if (permission == LocationPermission.deniedForever) {
+  //   ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+  //       content: Text('Location permissions are permanently denied, we cannot request permissions.')));
+  //   return false;
+  // }
 }
